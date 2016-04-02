2f67467a34304d1cbb278e7e5a22aab0b803ed38
Vinícius Krolow <krolow@gmail.com>
2016-04-01 22:39:46 -0300

    Avoid else by return early

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -1,11 +1,12 @@
 export default function (callback) {
 	if (document.readyState === 'complete') {
 		callback.bind(this);
-	} else {
-		document.addEventListener(
-			'DOMContentLoaded',
-			callback.bind(this),
-			false
-		);
+		return;
 	}
+
+	document.addEventListener(
+		'DOMContentLoaded',
+		callback.bind(this),
+		false
+	);
 }
