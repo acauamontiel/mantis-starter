214bb5b515c24a0b6a5ecc980031e037f65ae0da
Acauã Montiel <contato@acauamontiel.com.br>
2019-07-19 18:30:17 -0300

    improve ESLint

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
-console.time('Initialize');
+console.time('Initialize'); // eslint-disable-line no-console
 import App from './app/index';
 
 const app = new App();
 
 app.init(function () {
+	/*eslint no-console:0 */
 	console.log('%cMantis Starter', 'color: #338656; font: 50px sans-serif;');
 	console.timeEnd('Initialize');
+	/*eslint no-console:0 */
 });
