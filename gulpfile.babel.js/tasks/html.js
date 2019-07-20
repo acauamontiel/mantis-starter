2ca75ed8e3c2e2e69223a4923b292a98132558dc
Acauã Montiel <contato@acauamontiel.com.br>
2019-07-20 14:04:25 -0300

    add Pug linter

--- gulpfile.babel.js/tasks/html.js
+++ gulpfile.babel.js/tasks/html.js
@@ -1,12 +1,21 @@
 import {src, dest} from 'gulp';
 import plumber from 'gulp-plumber';
 import pug from 'gulp-pug';
+import pugLinter from 'gulp-pug-linter';
+import pugLintStylish from 'puglint-stylish';
 import {prod} from '../index';
 import {html} from '../paths';
 import content from '../../content.json';
 import {reload} from './serve';
 
 export default function htmlTask (done) {
+	if (!prod) {
+		src(html.watch)
+			.pipe(pugLinter({
+				reporter: pugLintStylish
+			}));
+	}
+
 	src(html.src)
 		.pipe(plumber())
 		.pipe(pug({
