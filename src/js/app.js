9b6e4b36947d217f8a0c765ce20d39b30363ae7a
Acauã Montiel <contato@acauamontiel.com.br>
2019-07-27 02:07:07 -0300

    load ESLint as package

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,8 @@ import App from './app/index';
 const app = new App();
 
 app.init(() => {
-	/*eslint no-console:0 */
+	/* eslint no-console:0 */
 	console.log('%cMantis Starter', 'color: #338656; font: 50px sans-serif;');
 	console.timeEnd('Initialize');
-	/*eslint no-console:0 */
+	/* eslint no-console:0 */
 });
